008d1aa0da84ec11127f6d54e29692508b3a01c4
Cho <eddy1001@naver.com>
2017-03-06 19:42:37 +0900

    - 수정사항 1. 스킬 완성 2. 버튼 이미지 새로 설정 3. UI 개선 4. 무기 강화 완성 5. 레벨업 완성 6. 포션 기능 완성

--- Assets/Script/Player_Move.cs
+++ Assets/Script/Player_Move.cs
@@ -19,6 +19,11 @@ public class Player_Move : MonoBehaviour {
     public Text Txt_HPPoint;
     public Text Txt_ExPoint;
 
+    public Image Skill1Button;
+    public Image Skill2Button;
+    public Image Skill3Button;
+    public Image PotionButton;
+
     public GameObject[] Mob;
 
     public int Level;
@@ -37,6 +42,10 @@ public class Player_Move : MonoBehaviour {
     public float HPPoint;
     public float HPMax;
     public float GodTime;
+    public float Skill1Cool;
+    public float Skill2Cool;
+    public float Skill3Cool;
+    public float PotionCool;
 
     public float[] MobExp;
 
@@ -65,42 +74,113 @@ public class Player_Move : MonoBehaviour {
 
         if (Player.transform.position.x < -7.0f)
             Player.transform.position = new Vector2(-7.0f, 0.0f);
+
+        if (Skill1Cool > 0.0f)
+        {
+            Skill1Cool -= Time.deltaTime;
+
+            Skill1Button.fillAmount = (2.0f - Skill1Cool) / 2.0f;
+        }
+        if (Skill2Cool > 0.0f)
+        {
+            Skill2Cool -= Time.deltaTime;
+
+            Skill2Button.fillAmount = (15.0f - Skill2Cool) / 15.0f;
+        }
+        if (Skill3Cool > 0.0f)
+        {
+            Skill3Cool -= Time.deltaTime;
+
+            Skill3Button.fillAmount = (30.0f - Skill3Cool) / 30.0f;
+        }
+        if (PotionCool > 0.0f)
+        {
+            PotionCool -= Time.deltaTime;
+
+            PotionButton.fillAmount = (10.0f - PotionCool) / 10.0f;
+        }
     }
 
     public void AttackStart()
     {
         Player.GetComponent<Animator>().SetFloat("Attack", 1);
+        GodMod = false;
     }
 
     public void AttackEnd()
     {
         Player.GetComponent<Animator>().SetFloat("Attack", 0);
+        GodMod = false;
     }
 
     public void Skill00Start()
     {
         Player.GetComponent<Animator>().SetFloat("Attack", 2);
+        GodMod = false;
     }
 
     public void Skill00End()
     {
         Player.GetComponent<Animator>().SetFloat("Attack", 0);
+        GodMod = false;
     }
 
     public void Skill01Do()
     {
-        Player.GetComponent<Animator>().SetFloat("Attack", 3);
+        if (Skill1Cool <= 0.0f)
+        {
+            Player.GetComponent<Animator>().SetFloat("Attack", 3);
+            GodMod = false;
+
+            Skill1Cool = 2.0f;
+        }
     }
 
     public void Skill02Start()
     {
-        Player.GetComponent<Animator>().SetFloat("Attack", 4);
-        Skill02Time = 20;
+        if (Skill2Cool <= 0.0f)
+        {
+            Player.GetComponent<Animator>().SetFloat("Attack", 4);
+            Skill02Time = 20;
+            GodMod = false;
+
+            Skill2Cool = 15.0f;
+        }
     }
 
     public void Skill02End()
     {
         Player.GetComponent<Animator>().SetFloat("Attack", 0);
+        GodMod = false;
+    }
+
+    public void Skill03Start()
+    {
+        if (Skill3Cool <= 0.0f)
+        {
+            Player.GetComponent<Animator>().SetFloat("Attack", 5);
+            GodMod = true;
+
+            Skill3Cool = 30.0f;
+        }
+    }
+
+    public void Skill03End()
+    {
+        Player.GetComponent<Animator>().SetFloat("Attack", 0);
+        GodMod = false;
+    }
+
+    public void PotionStart()
+    {
+        if (PotionCool <= 0.0f)
+        {
+            HPPoint = HPMax;
+            Sld_HPPoint.value = HPPoint / HPMax;
+            Txt_HPPoint.text = HPPoint.ToString() + " / " + HPMax.ToString();
+
+            PotionCool = 10.0f;
+        }
     }
 
     public void MoveLeftStart()
@@ -167,7 +247,7 @@ public class Player_Move : MonoBehaviour {
             float MobRange = Vector2.Distance(Mob[i].transform.position, Player.transform.position);
             if (Facing && MobFacing > -0.5f || !Facing && MobFacing < 0.5f)
             {
-                if (MobRange < 1.75)
+                if (MobRange < 2.75)
                 {
                     int FinalPower = (int)Mathf.Round(Power * Random.Range(0.75f, 1.25f) * 1.5f);
                     PoolDam.GetComponent<PoolDam_Move>().MakeDam(Mob[i].transform.position.x, 0.75f, FinalPower.ToString(), Colors[0]);
@@ -272,6 +352,30 @@ public class Player_Move : MonoBehaviour {
             Skill02End();
     }
 
+    public void Skill03()
+    {
+        for (int i = 0; i < Mob.Length; i++)
+        {
+            if (Mob[i].GetComponent<Mob_Move>().HPPoint <= 0)
+                continue;
+
+            int FinalPower = (int)Mathf.Round(Power * Random.Range(0.75f, 1.25f) * 15.0f);
+            PoolDam.GetComponent<PoolDam_Move>().MakeDam(Mob[i].transform.position.x, 0.75f, FinalPower.ToString(), Colors[0]);
+
+            Mob[i].GetComponent<Mob_Move>().HPPoint -= FinalPower;
+            if (Mob[i].GetComponent<Mob_Move>().HPPoint <= 0)
+            {
+                Mob[i].GetComponent<Mob_Move>().Death();
+                MobKill(0);
+                PoolDam.GetComponent<PoolDam_Move>().MakeDam(Mob[i].transform.position.x, 0.15f, "+ 10금", Colors[1]);
+            }
+            else
+            {
+                AttackMob(i);
+            }
+        }
+    }
+
     public void AttackMob(int MobNum)
     {
         Mob[MobNum].GetComponent<Mob_Move>().MoveTime = 0.0f;
@@ -336,6 +440,11 @@ public class Player_Move : MonoBehaviour {
 
             Power = 10 + Level * 2;
             Txt_Power.text = Power.ToString();
+
+            HPMax += 10;
+            HPPoint = HPMax;
+            Sld_HPPoint.value = HPPoint / HPMax;
+            Txt_HPPoint.text = HPPoint.ToString() + " / " + HPMax.ToString();
         }
         Sld_ExPoint.value = ExPoint / ExMax;
         Txt_ExPoint.text = ExPoint.ToString() + " / " + ExMax.ToString();
